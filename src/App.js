bee6e823a2d1489667f1c1c8b3fab9c1d2ca101a
jwParkDev <110954895+jwParkDev@users.noreply.github.com>
2022-12-16 19:04:28 +0900

    style: apply styled-components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import { Counter } from './Counter.js';
 import { Provider } from 'react-redux';
 import { store } from './Store';
+import styled from 'styled-components';
+
+const MainBody = styled.div`
+  width:100%;
+`;
 
 function App() {
   return (
-    <div>
+    <MainBody>
       <Provider store={store}>
         <Counter />
       </Provider>
-    </div>
+    </MainBody>
   );
 }
 
--- src/Counter.js
+++ src/Counter.js
@@ -1,15 +1,28 @@
 import { useDispatch, useSelector } from "react-redux"
 import { CounterSlice } from "./slice";
+import styled from "styled-components";
+
+const CounterDiv = styled.div`
+  width:1000px;
+  margin:0 auto;
+  display:flex;
+  justify-content: center;
+  align-items: center;
+
+  & > button {
+    margin: 0 10px
+  }
+`;
 
 export const Counter = () => {
   const dispatch = useDispatch();
   const count = useSelector(state => state.counter.v);
 
   return (
-    <div>
+    <CounterDiv>
       <button onClick={() => dispatch(CounterSlice.actions.down(1))}>down</button>
-      <div>{count}</div>
+      <div>{`Counter : ${count}`}</div>
       <button onClick={() => dispatch(CounterSlice.actions.up(1))}>up</button>
-    </div>
+    </CounterDiv>
   )
 }
